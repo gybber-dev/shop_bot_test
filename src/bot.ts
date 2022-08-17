1e8875ed2d7c8375f9d36d9f2934facadb439aaa
Azat Faezov <farscince@gmail.com>
2022-08-17 22:12:49 +0300

    project settings: linter

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,15 @@
-import { Bot } from 'grammy'
-import * as process from "process";
-import dotenv from 'dotenv'
+import { Bot } from 'grammy';
+import * as process from 'process';
+import dotenv from 'dotenv';
 
-dotenv.config()
-const { TOKEN } = process.env
+dotenv.config();
+const { TOKEN } = process.env;
 
 if (TOKEN) {
-    const bot = new Bot(TOKEN)
-    bot.command('start', ctx => ctx.reply('Started'))
+  const bot = new Bot(TOKEN);
+  bot.command('start', ctx => ctx.reply('Started'));
 
-    bot.on('message', ctx => ctx.reply('Got message'))
-    console.log('start the bot')
-    bot.start()
+  bot.on('message', ctx => ctx.reply('Got message'));
+  console.log('start the bot');
+  bot.start();
 }
