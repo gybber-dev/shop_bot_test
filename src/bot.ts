ee80f7df1d423a4d46ae2e96d581d1a64cf7924f
Azat Faezov <farscince@gmail.com>
2022-08-28 16:48:10 +0300

    commands

--- src/bot.ts
+++ src/bot.ts
@@ -3,36 +3,31 @@ import * as process from 'process';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const { TOKEN } = process.env;
 
-if (TOKEN) {
-  const bot = new Bot(TOKEN);
+const run = async () => {
+  const { TOKEN } = process.env;
+  if (TOKEN) {
+    const bot = new Bot(TOKEN);
+    bot.command('start', ctx => ctx.reply('Started!'));
 
-  bot.command('start', ctx => ctx.reply('Started!'));
+    /** COMMANDS **/
+    /* https://grammy.dev/guide/commands.html#usage */
 
-  /* FILTER QUERIES */
-  // bot.on('message::url', (ctx) => ctx.reply('url2'));
-  // bot.on('message:text', (ctx) => ctx.reply('Text!'));
-  // bot.on('message:photo', (ctx) => ctx.reply('photo!'));
-  // bot.on('edit', ctx => ctx.reply('edited')); // any message or channel post edit
+    // set commands set for your bot
+    await bot.api.setMyCommands([
+      { command: 'start', description: 'Start the bot' },
+      { command: 'help', description: 'Show help text' },
+      { command: 'settings', description: 'Open settings' },
+    ]);
 
-  // combine with OR
-  bot.on(['::email', '::url'], ctx => ctx.reply('email or url'));
-  // combine with AND
-  bot.on(':photo').on('::hashtag', ctx => ctx.reply('Matches photos that contain a hashtag in a photo\'s caption'));
+    // define command's arguments
+    bot.command('add', (ctx) => {
+      // `item` will be "apple pie" if a user sends "/add apple pie".
+      const args = ctx.match;
+      console.log('you sent command "add" with the following arguments', args);
+    });
+    await bot.start();
+  }
+};
 
-  // Filtering by User Properties
-  bot.on('message').filter(
-      async (ctx) => {
-        const user = await ctx.getAuthor();
-        console.log(user);
-        return user.status === 'creator' || user.status === 'administrator';
-      },
-      async (ctx) => {
-        // Handles messages from creators and admins.
-        await ctx.reply('Hello, my master!');
-      },
-  );
-
-  bot.start();
-}
+run();
